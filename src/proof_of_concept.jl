f699044d518440b05ade4f544f260922897cfaf2
Nelson Barnett <nelsonvbarnett@gmail.com>
2023-01-19 16:18:42 -0500

    Remove MATLAB comparison in proof of concept

--- src/proof_of_concept.jl
+++ src/proof_of_concept.jl
@@ -9,31 +9,10 @@ using DelimitedFiles
 using FileIO
 using Statistics
 
+### Setup
 stimgen = UniformPrior(; max_freq=13e3, n_bins=8, min_bins=3, max_bins=6)
 
 Γ = 2
-
-### Repeatable section with saved stimuli and spectral representation of Buzzing Tone.
-
-# Because MATLAB and Julia wav2spect have numerically different outputs.
-s_f = readdlm("../wav2spect_output.csv", ',', Float64, '\n')
-s = s_f[:, 1]
-
-# To compare directly with MATLAB using same stimuli.
-stimuli_matrix = readdlm("../stimuli_matrix_buzzing.csv", ',', Float64, '\n')
-
-target_signal = 10 * log10.(s)
-binned_target_signal = spect2binnedrepr(stimgen, target_signal)
-
-responses_synth, = subject_selection_process(stimuli_matrix', binned_target_signal)
-
-reconstruction_synth_lr = gs(responses_synth, stimuli_matrix')
-r_static_lr = cor(reconstruction_synth_lr, binned_target_signal)
-
-reconstruction_synth_cs = cs(responses_synth, stimuli_matrix', Γ)
-r_static_cs = cor(reconstruction_synth_cs, binned_target_signal)
-
-### Using Julia wav2spect.
 
 audio_file = "ATA/ATA_Tinnitus_Buzzing_Tone_1sec.wav"
 
@@ -42,16 +21,7 @@ audio = wav2spect(audio_file)
 target_signal = 10 * log10.(audio)
 binned_target_signal = spect2binnedrepr(stimgen, target_signal)
 
-responses_synth, = subject_selection_process(stimuli_matrix', binned_target_signal)
-
-reconstruction_synth_lr = gs(responses_synth, stimuli_matrix')
-r_Julia_lr = cor(reconstruction_synth_lr, binned_target_signal)
-
-reconstruction_synth_cs = cs(responses_synth, stimuli_matrix', Γ)
-r_Julia_cs = cor(reconstruction_synth_cs, binned_target_signal)
-
 ### Loop for statistics on randomly generated stimuli.
-
 n = 30
 r_loop_lr = zeros(n, 1)
 r_loop_cs = zeros(n, 1)
@@ -72,10 +42,7 @@ r_std_lr = std(r_loop_lr)
 r_mean_cs = mean(r_loop_cs)
 r_std_cs = std(r_loop_cs)
 
-println("Linear r using read-in stimuli matrix and target signal = $(r_static_lr[1])")
-println(
-    "Linear r using read-in stimuli matrix and Julia wav2spect target signal = $(r_Julia_lr[1])",
-)
+### Print results
 println("Mean linear r using a generated stimuli matrix for n = $n is $r_mean_lr")
 println(
     "Standard deviation of linear r using a generated stimuli matrix for n = $n is $r_std_lr",
@@ -83,10 +50,6 @@ println(
 
 println("----------")
 
-println("CS r using read-in stimuli matrix and target signal = $(r_static_cs[1])")
-println(
-    "CS r using read-in stimuli matrix and Julia wav2spect target signal = $(r_Julia_cs[1])"
-)
 println("Mean CS r using a generated stimuli matrix for n = $n is $r_mean_cs")
 println(
     "Standard deviation of CS r using a generated stimuli matrix for n = $n is $r_std_cs"
